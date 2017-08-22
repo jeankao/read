dc2098f50b69a6f13d310000d5eefd3ea8932910
koma <koma@mail.nksh.tp.edu.tw>
2017-08-22 00:16:22 -0400

    database

--- read/settings.py
+++ read/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'read.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'read',
+    'USER': 'read',
+    'PASSWORD': '1234',
+    'HOST': 'localhost',
+    'PORT': '',  
+  
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
