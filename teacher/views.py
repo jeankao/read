c07412b5d3b3c3edb497a8b17ebec337b13d5155
t005 <t005@mail.nksh.tp.edu.tw>
2017-10-14 16:37:40 -0400

    random group

--- teacher/views.py
+++ teacher/views.py
@@ -1513,7 +1513,7 @@ def speculation_annotation_edit(request, forum_id, content_id):
 def speculation_group(request, classroom_id, forum_id):
     title = SpeculationWork.objects.get(id=forum_id).title
     speculation = SpeculationClass.objects.get(forum_id=forum_id, classroom_id=classroom_id)
-    groups = ClassroomGroup.objects.filter(classroom_id=speculation.classroom_id)
+    groups = ClassroomGroup.objects.filter(classroom_id=speculation.classroom_id).order_by("-id")
     return render_to_response('teacher/speculation_group.html',{'speculation': speculation, 'groups':groups, 'title':title}, context_instance=RequestContext(request))
 
 def speculation_group_set(request):
