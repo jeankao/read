dfd7b2e885199c74c5cba022b3903db109e50bab
t005 <t005@mail.nksh.tp.edu.tw>
2017-11-16 20:03:08 -0500

    video time

--- teacher/helper.py
+++ teacher/helper.py
@@ -27,9 +27,6 @@ class VideoLogHelper:
                     length = int(
                         (event.publish - start_log_time).total_seconds())
                     tto = tfrom + length                     
-                    #tmp = time.split(":")
-                    #tto = int(tmp[0]) * 3600 + int(tmp[1]) * 60 + int(tmp[2])
-                    #length = tto - tfrom
                     videos.append({'stamp': str(localtime(start_log_time).strftime(
                         "%Y-%m-%d %H:%M:%S")), 'from': tfrom, 'to': tto, 'length': length, 'duration': 300})
                     searching = False
