eda1136e407506e3d5431e0526abc47c1e38153a
Jean, Hue-Ching Kao <koma@mail.nksh.tp.edu.tw>
2019-02-11 18:31:08 +0800

    update

--- account/templatetags/tag.py
+++ account/templatetags/tag.py
@@ -80,10 +80,10 @@ def assistant(user_id):
 def number(youtube):
     number_pos = youtube.find("v=")
     if number_pos > 0:
-        number = url[number_pos+2:number_pos+13]
+        number = youtube[number_pos+2:number_pos+13]
     else :
-        number_pos = url.find("youtu.be/")
-        number = url[number_pos+9:number_pos+20]
+        number_pos = youtube.find("youtu.be/")
+        number = youtube[number_pos+9:number_pos+20]
     return number 
 
 @register.filter()
