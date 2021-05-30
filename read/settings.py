7cb0ab8352c920a5e93c6deecb83f38457b87c6f
jeankao <koma@mail.nksh.tp.edu.tw>
2021-05-30 13:21:59 +0800

    update

--- read/settings.py
+++ read/settings.py
@@ -79,17 +79,17 @@ WSGI_APPLICATION = 'read.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #'NAME': 'read',
-    #'USER': 'read',
-    #'PASSWORD': '1234',
-    #'HOST': 'localhost',
-    #'PORT': '',  
-  
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'read',
+    'USER': 'read',
+    'PASSWORD': '1234',
+    'HOST': 'localhost',
+    'PORT': '',  
+  
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
