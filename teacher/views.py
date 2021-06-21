d49fed6c1facb420563000ede555bfc573083433
jeankao <koma@mail.nksh.tp.edu.tw>
2021-06-22 04:27:26 +0800

    update

--- teacher/views.py
+++ teacher/views.py
@@ -2379,12 +2379,14 @@ def team_group(request, classroom_id, team_id):
     teamclass = TeamClass.objects.get(team_id=team_id, classroom_id=classroom_id)
     try:
         group = ClassroomGroup.objects.get(id=teamclass.group)
-        teamworks = TeamWork.objects.filter(classroom_id=classroom_id)
-        for teamwork in teamworks:
-            teamwork.group = group
-            teamwork.save()
+
     except ObjectDoesNotExist:
         group = ClassroomGroup(title="不分組", id=0)
+    teamworks = TeamWork.objects.filter(classroom_id=classroom_id)
+    for teamwork in teamworks:
+        teamwork.group = group
+        teamwork.save()
+
     return render(request,'teacher/team_group.html',{'team_id': team_id, 'teamgroup': group, 'groups':groups, 'classroom':classroom, 'group_list':group_list})
 
 # 影片觀看時間統計
