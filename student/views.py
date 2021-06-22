d9497fd89898aba8667279c13de4a4011bc9a160
jeankao <koma@mail.nksh.tp.edu.tw>
2021-06-22 11:18:18 +0800

    update

--- student/views.py
+++ student/views.py
@@ -1224,7 +1224,7 @@ class TeamContentListView(ListView):
         if self.kwargs['grouping'] == "0":
             group_id = 0
         else:
-            enrolls = StudentGroup.objects.filter(group_id=self.kwargs['grouping'], group=self.kwargs['stage_id'])
+            enrolls = StudentGroup.objects.filter(group_id=self.kwargs['grouping'], group=self.kwargs['stage'])
             group_id = enroll[0].group
         publish = self.kwargs['publish']
         user_ids = []        
